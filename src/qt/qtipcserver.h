a02880bb3aaddd65ca275d36a7d30db47ccacab7
equinoxmanagement <vendefer@ip-192-168-1-6.is74.loc>
2014-09-12 20:44:40 +0600

    1
    1

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define EquinoxCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "EquinoxCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
